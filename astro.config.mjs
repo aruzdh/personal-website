893233d9cb1457748c52a95ed16032c24defa3c8
Aru <aruzdh@gmail.com>
2026-06-25 14:46:09 -0600

    feat: change font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://aruzdh.dev',
   integrations: [sitemap()],
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: 'Plus Jakarta Sans',
+      cssVariable: '--font-plus-jakarta-sans',
+      weights: ['200..800'],
+      styles: ['normal', 'italic'],
+    },
+  ]
 });
